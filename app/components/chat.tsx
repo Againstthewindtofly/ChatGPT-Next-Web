84d32de8560ce2d564987e0aff5376a46abc9000
Againstthewindtofly <46886020+Againstthewindtofly@users.noreply.github.com>
2023-10-27 14:26:46 +0800

    Update chat.tsx
    add ipc info

--- app/components/chat.tsx
+++ app/components/chat.tsx
@@ -1284,6 +1284,8 @@ function _Chat() {
             onClick={() => doSubmit(userInput)}
           />
         </div>
+
+        < div style = { { display : 'inline-block' , VerticalAlign : 'middle' , fontSize : '12px' , textAlign : 'center' , width : '100%' , position : ' absolute' } } > <样式 = { { color : '#585e6d' , textDecoration : 'none' , cursor : 'pointer' } } href = "https://beian.miit.gov.cn/" target = "_blank" > ICP备案 豫ICP备2021006359号-1 </a> </div>
       </div>
 
       {showExport && (
